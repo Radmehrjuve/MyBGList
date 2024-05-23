23eea02ae7f169897d433f751a3ff4b2086a2f12
Radmehr <Radmehrvosooghi@gmail.com>
2024-05-23 15:37:39 +0330

    .

--- MyBGList/BoardGame.cs
+++ MyBGList/BoardGame.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public int? Year { get; set; }
-        public int MinPlayer { get; set; }
-        public int MaxPlayer { get; set; }
+        public int? MinPlayer { get; set; }
+        public int? MaxPlayer { get; set; }
     }
 }
